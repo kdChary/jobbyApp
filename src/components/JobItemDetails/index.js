f93aca5431950e25d1f80c9bd1d63c003e377ca3
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-05 13:46:06 +0000

    jobDetails coded [nonsense]

--- src/components/JobItemDetails/index.js
+++ src/components/JobItemDetails/index.js
@@ -2,7 +2,12 @@ import {Component} from 'react'
 import {Redirect} from 'react-router-dom'
 import Cookies from 'js-cookie'
 
+import {FaStar} from 'react-icons/fa'
+import {TiLocation} from 'react-icons/ti'
+import {BsBriefcaseFill} from 'react-icons/bs'
+
 import './index.css'
+import Header from '../Header'
 
 const apiConstant = {
   initial: 'INITIAL',
@@ -28,6 +33,7 @@ class JobItemDetails extends Component {
   })
 
   modifyJobDetails = data => ({
+    title: data.title,
     companyLogoUrl: data.company_logo_url,
     companyWebsiteUrl: data.company_website_url,
     employmentType: data.employment_type,
@@ -82,16 +88,92 @@ class JobItemDetails extends Component {
       this.setState({fetchStatus: apiConstant.success})
 
       const modifiedData = this.modifyData(data)
-      this.onRequestSuccess(modifiedData)
-
-      console.log(modifiedData)
+      this.onRequestSuccess(modifiedData.jobDetails)
     } else {
       this.setState({fetchStatus: apiConstant.failure})
     }
   }
 
+  renderJobItemDetail = () => {
+    const {jobItemData} = this.state
+
+    const {
+      title,
+      companyLogoUrl,
+      employmentType,
+      companyWebsiteUrl,
+      jobDescription,
+      location,
+      packagePerAnnum,
+      rating,
+      skills,
+      lifeAtCompany,
+    } = jobItemData
+
+    return (
+      <>
+        <div className="selected-job-details-header">
+          <div className="selected-job-company-info">
+            <img
+              src={companyLogoUrl}
+              alt="company logo"
+              className="selected-job-logo"
+            />
+
+            <div className="selected-job-title-rating-info">
+              <h5 className="selected-job-title">{title}</h5>
+
+              <div className="selected-job-rating-card">
+                <FaStar className="star-icon" />
+
+                <p className="selected-job-rating">{rating}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="selected-job-location-package-card">
+            <ul className="selected-job-location-list">
+              <li className="selected-job-list-item">
+                <TiLocation className="selected-job-details-icon" />
+                <p className="selected-job-details-location-text">{location}</p>
+              </li>
+
+              <li className="selected-job-list-item">
+                <BsBriefcaseFill className="selected-job-details-icon" />
+                <p className="selected-job-details-location-text">
+                  {employmentType}
+                </p>
+              </li>
+            </ul>
+
+            <p className="selected-job-package">{packagePerAnnum}</p>
+          </div>
+        </div>
+        <hr className="line" />
+
+        <div className="selected-job-details-footer ">
+          <h4 className="selected-job-description-title">Description</h4>
+          <p className="selected-job-description">{jobDescription}</p>
+        </div>
+      </>
+    )
+  }
+
   render() {
-    return <h1>Test</h1>
+    const token = Cookies.get('jwt_token')
+
+    if (token === undefined) {
+      return <Redirect to="/login" />
+    }
+
+    return (
+      <div className="selected-job-bg">
+        <Header />
+        <div className="selected-job-details-container">
+          {this.renderJobItemDetail()}
+        </div>
+      </div>
+    )
   }
 }
 
